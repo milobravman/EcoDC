9bce0fee30effca73beecf942705fe3824d1499f
Milo_Lynch <Milobravman20@gmail.com>
2022-06-27 23:14:13 -0400

    reduced CSS added matrials ui components

--- ecodc/src/App.js
+++ ecodc/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h3>Eco Dc</h3>
+        <Header/>
       </header>
     </div>
   );
