337ad1aa4cd9707278968e8aab3f1ac0d1be6a02
Milo_Lynch <Milobravman20@gmail.com>
2022-07-02 21:23:20 -0400

    onHover is super broken

--- ecodc/src/Menu.js
+++ ecodc/src/Menu.js
@@ -8,10 +8,13 @@ export default function BasicMenu() {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
+    console.log(event.currentTarget);
+    console.log(anchorEl);
     setAnchorEl(event.currentTarget);
+    console.log(anchorEl);
   };
   const handleClose = (event) => {
-    console.log("Hello")
+    console.log("handleClose fires")
     setAnchorEl(null);
   };
 
@@ -20,30 +23,33 @@ export default function BasicMenu() {
   }
 
   return (
-    <div id="menu-div">
+    <div id="menu-div"
+    >
       <Button
         id="basic-button"
         aria-controls={open ? 'basic-menu' : undefined}
         aria-haspopup="true"
         aria-expanded={open ? 'true' : undefined}
-        onMouseEnter={handleClick}
-        onMouseLeave={console.log("foo")}
-      >
+        onMouseOver={handleClick}
+       >
         Recorces
       </Button>
-      <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-        }}
-      >
-        <MenuItem onClick={HandleRecycle}>Recycling</MenuItem>
-        <MenuItem onClick={handleClose}>Empty</MenuItem>
-        <MenuItem onClick={handleClose}>Empty</MenuItem>
-      </Menu>
+      <div id = "temp">
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+
+          MenuListProps={{
+            'aria-labelledby': 'basic-button',
+          }}
+        >
+          <MenuItem onClick={HandleRecycle}>Recycling</MenuItem>
+          <MenuItem onClick={handleClose}>Empty</MenuItem>
+          <MenuItem onClick={handleClose}>Empty</MenuItem>
+        </Menu>
+      </div>
     </div>
   );
 }
