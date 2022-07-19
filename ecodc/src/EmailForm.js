46cac5a90b72cf3a083feb740ad8edce5fe2d73b
Milo_Lynch <Milobravman20@gmail.com>
2022-07-19 16:55:44 -0400

    got text field visible on home page

---
+++ ecodc/src/EmailForm.js
@@ -0,0 +1,11 @@
+import TextField from '@mui/material/TextField';
+
+function EmailForm() {
+    return(
+        <div id= "textField">
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </div>
+    
+    )
+}
+export default EmailForm;
--- ecodc/src/Home.js
+++ ecodc/src/Home.js
@@ -1,12 +1,16 @@
 import "./App.css"
 import Menu from './Menu';
+import EmailForm from './EmailForm';
 
 function Home() {
     return (
-        <header className="App-header">
-            <Menu id= "menu" className="App-head"/>
-            <h3 id= "title" className='App-head'>Eco Dc</h3>
-        </header>
+        <>
+            <header className="App-header">
+                <Menu id= "menu" className="App-head"/>
+                <h3 id= "title" className='App-head'>Eco Dc</h3>
+            </header>
+            <EmailForm/>
+        </>
     )
 }
 
