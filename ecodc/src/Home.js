bfa9ce0ef8e21f3143ec6d95e2d166c76ec2411f
Milo_Lynch <Milobravman20@gmail.com>
2022-08-02 18:23:36 -0400

    landing page first commit

--- ecodc/src/Home.js
+++ ecodc/src/Home.js
@@ -1,14 +1,16 @@
 import "./App.css"
 import Menu from './Menu';
 import EmailForm from './EmailForm';
+import LandingPage from './LandingPage'
 
 function Home() {
     return (
         <>
             <header className="App-header">
-                <Menu id= "menu" className="App-head"/>
+                {/* <Menu id= "menu" className="App-head"/> */}
                 <h3 id= "title" className='App-head'>Eco Dc</h3>
             </header>
+            <LandingPage/>
             <EmailForm/>
         </>
     )
---
+++ ecodc/src/LandingPage.js
@@ -0,0 +1,9 @@
+function LandingPage() {
+    return(
+        <>
+            <h3 id = "LandingTitle">Pick Your Battle, Then Keep It Simple</h3>
+        </>
+    )
+}
+
+export default LandingPage;
