eb729cb75c4dd4be511d56bd69345862688b428d
Milo_Lynch <Milobravman20@gmail.com>
2022-08-03 16:47:58 -0400

    homepage no longer primordial html soup

--- ecodc/src/Components/MuiCards.js
+++ ecodc/src/Components/MuiCards.js
@@ -5,71 +5,16 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
 import Grid from '@mui/material/Grid';
-import FormLabel from '@mui/material/FormLabel';
-import FormControl from '@mui/material/FormControl';
-import FormControlLabel from '@mui/material/FormControlLabel';
-
-import RadioGroup from '@mui/material/RadioGroup';
-import Radio from '@mui/material/Radio';
-import Paper from '@mui/material/Paper';
 
 
 
 
 export default function MultiActionAreaCard() {
-    const [spacing, setSpacing] = React.useState(2);
 
-    const handleChange = (event) => {
-      setSpacing(Number(event.target.value));
-    };
+
   
     return (
-
-
-<Grid item xs={12}>
-
-    <Grid container justifyContent="center" spacing={spacing}>
-                {[0, 1, 2, 3].map((value) => (
-                    <Grid key={value} item>
-                    <Paper
-                        sx={{
-                        height: 140,
-                        width: 100,
-                        backgroundColor: (theme) =>
-                            theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-                        }}
-                    />
-                    </Grid>
-                ))}
-                </Grid>
-            <Grid item xs={12}>
-                <Paper sx={{ p: 2 }}>
-                <Grid container>
-                    <Grid item>
-                    <FormControl component="fieldset">
-                        <FormLabel component="legend">spacing</FormLabel>
-                        <RadioGroup
-                        name="spacing"
-                        aria-label="spacing"
-                        value={spacing.toString()}
-                        onChange={handleChange}
-                        row
-                        >
-                        {[0, 0.5, 1, 2, 3, 4, 8, 12].map((value) => (
-                            <FormControlLabel
-                            key={value}
-                            value={value.toString()}
-                            control={<Radio />}
-                            label={value.toString()}
-                            />
-                        ))}
-                        </RadioGroup>
-                    </FormControl>
-                    </Grid>
-                </Grid>
-                </Paper>
-                </Grid>
-
+        <Grid container justifyContent="center" >
                 <Card className="float-child" sx={{ maxWidth: 345 }}>
                 <CardActionArea>
                     <CardMedia
@@ -168,7 +113,7 @@ export default function MultiActionAreaCard() {
                     </Button>
                 </CardActions>
                 </Card>
-</Grid>        
+        </Grid>        
 
 
   );
