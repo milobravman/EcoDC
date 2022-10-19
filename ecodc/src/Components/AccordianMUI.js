3696fe068de15e8c892811a9508201048c500fc8
Milo Bravman <Milobravman20@gmail.com>
2022-10-18 22:29:09 -0400

    text changes

--- ecodc/src/Components/AccordianMUI.js
+++ ecodc/src/Components/AccordianMUI.js
@@ -7,6 +7,8 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 // import testImage from './logo192.png'
 import InteralAccordian from './InterialAccordian'
 import More from './InternialAccordian2'
+import { color } from '@mui/system';
+import { green } from '@mui/material/colors';
 
 
 
@@ -38,59 +40,69 @@ export default function ControlledAccordions() {
           aria-controls="panel1bh-content"
           id="panel1bh-header"
         >
-          <Typography sx={{ width: '33%', flexShrink: 0 }}>
-          <span className='bold underline large'>Recycle Better</span>
-          </Typography>
-          <Typography > 
-            {/* <span className='regular '> The Goal: </span>
-            <span className='regular blue'>Everything that </span>
-            <span className='regular blue bold underline'>can</span>
-            <span className='regular blue'> get recycled goes into the Blue Bin. </span>
-            <span className='regular red'>Whatever could get the Blue Bin</span>
-            <span className='regular red bold italics'> banished to trash </span>
-            <span className='regular red'>stays out</span> */}
-            <span className='underline bold regular blue' id ='keep'> Keep non-recyclables out of your blue bin.</span>
-            <span className='italics bold regular red' id ='non-r'> Non-recyclables spoil the bin AND MAYBE THE WHOLE TRUCK!</span>
-
-          </Typography>
+            <Typography sx={{ width: '33%', flexShrink: 0, marginTop: '60px', marginBottom: '60px' }}>
+              <span className='bold underline large'>Recycle Better</span>
+            </Typography>
+            <Typography sx={{ marginTop: '60px', marginBottom: '60px', fontSize: 22}}> 
+              {/* <span className='underline bold regular blue' id ='keep'> Keep non-recyclables out of your blue bin.</span>
+              <span className='italics bold regular red' id ='non-r'> Non-recyclables spoil the bin AND MAYBE THE WHOLE TRUCK!</span> */}
+              DC's recycling system is only as good as what we put into it. Learn what goes in and what stays out. It's worth the effort!
+            </Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
             {/* test goes here */}
-              <audio id='recycleAudio' src='audio/ecoDc_audio.mp3' controls>
-              </audio>
-            <div id = "AccMui-goesIn-div">
+              {/* <audio id='recycleAudio' src='audio/ecoDc_audio.mp3' controls>
+              </audio> */}
               <h3 className='green'>Goes in!</h3>
+              <Typography className='green' sx={{fontSize: '22px'}}>
+                 Put in the stuff that our recycling system can safely and effectively turn into materials for reuse. 
+              </Typography>
+            <div id = "AccMui-goesIn-div">
               <ol>
                 <li>
-                  <span className='bold'> The usual stuff: </span>
-                  <span>paper, cardboard, aluminum or tin cans, plastic bottles & jugs, milk cartons, glass. Needs to be reasonably clean.</span>
+                  <span className='bold'>Paper:</span>
+                  <span> paper bags, newspaper, junk mail (including glossy mailers), non-padded envelopes (including the ones with plastic windows), paperback and hardback books, magazines & catalogues.</span>
                 </li>
                 <li>
-                  <span className='bold'> Pizza boxes </span>
-                  <span>(first remove crusts and small plastic items—grease is fine!)</span>
+                  <span className='bold'> Cardboard boxes: </span>
+                  <span>The system is designed for flattened and dry cardboard, so break them down and keep them from getting rained on.</span>
                 </li>
                 <li>
-                  <span className='bold'> All junk mail, </span>
-                  <span>even the glossy stuff</span>
+                  <span className='bold'>Pizza boxes: </span>
+                  <span>Remove crusts and small plastic items—grease is fine! </span>
                 </li>
                 <li>
-                  <span className='bold'>Paper towel rolls and toilet paper rolls</span>
-                  <span></span>
+                  <span className='bold'>Inner cardboard roll from toilet paper and paper towel rolls.</span>
+                  <span> </span>
                 </li>
                 <li>
-                  <span className='bold'>Coffee cups </span>
-                  <span>(plastic lids removed) </span>
+                  <span className='bold'>Disposable coffee cups. </span>
+                  <span>The system is designed to sort most containers by shape, so don't crush them. (Do remove the plastic lid.)</span>
                 </li>
-
-
-                <li>
+                {/* <li>
                   <span className='bold'> Any rigid plastic </span>
                   <span >that isn't #3, including most takeout containers and shampoo bottles, of any color. <a href='./images/ridged_plastic.jpg' target="_blank">See Pictures</a> </span>
+                </li> */}
+                <li>
+                  <span className='bold'>  Milk (or juice) cartons. </span>
+                  <span>Don't crush them.  (Do re-attach the plastic cap).</span>
                 </li>
                 <li>
-                  <span className='bold'> Plastic bottle caps</span>
-                  <span> (just put them back on bottle)</span>
+                  <span className='bold'> Plastic drink bottles. Plastic soap, detergent, or shampoo bottles. </span>
+                  <span> Don't crush them. (Do re-attach their plastic caps). </span>
+                </li>
+                <li>
+                  <span className='bold'> Uncrushed, clean  plastic clamshell containers. </span>
+                  <span>Remove the inner pad. See pictures for common examples.   </span>
+                </li>
+                <li>
+                  <span className='bold'> Clean plastic take-out containers. </span>
+                  <span> Don't crush.  </span>
+                </li>
+                <li>
+                  <span className='bold'> Aluminum or tin food/drink cans.   </span>
+                  <span> Don't crush. No paint cans. </span>
                 </li>
               </ol>
             </div>
@@ -98,8 +110,11 @@ export default function ControlledAccordions() {
               <h5 id="photoCycleTitle">Ridged Plastic</h5>
               <img id="cyclePhoto" src="./images/ridged_plastic.jpg" alt=''></img>
             </div> */}
+            <h3 className='red'>Stays out!</h3>
+            <Typography className='red' sx={{fontSize: '22px'}}>
+              Keep out the wrong stuff to prevent contamination, slowdowns, extra costs, and worker injuries.
+            </Typography>
             <div>
-              <h3 className='red'>Stays out!</h3>
               <ol>
                 <li>
                   <span className='bold'> Plastic bags. </span>
@@ -125,7 +140,7 @@ export default function ControlledAccordions() {
                 </li>
                 <li>
                   <span className='bold underline'> Anything you're unsure of</span>
-                  <span className='otherBlue'>. Don't guess! When in doubt, throw it out.</span>
+                  <span className='otherBlue' >. Don't guess! When in doubt, throw it out.</span>
                 </li>
               </ol>
             </div>
@@ -138,11 +153,11 @@ export default function ControlledAccordions() {
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel2bh-content"
           id="panel2bh-header">
-          <Typography sx={{ width: '33%', flexShrink: 0 }}>
+          <Typography sx={{ width: '33%', flexShrink: 0, marginTop: '60px', marginBottom: '60px'  }}>
             <span className='bold underline large'>Recycle Bigger</span>
           </Typography>
            
-          <Typography >
+          <Typography sx={{ marginTop: '60px', marginBottom: '60px'}}>
             <span className='regular green bold'>Beyond the bin, </span>
             <span className='regular'>there's a whole world of opportunities to recycle </span>
             <span className='regular green'>food waste, </span>
@@ -165,10 +180,10 @@ export default function ControlledAccordions() {
           aria-controls="panel3bh-content"
           id="panel3bh-header"
         >
-          <Typography sx={{ width: '33%', flexShrink: 0 }}>
+          <Typography sx={{ width: '33%', flexShrink: 0, marginTop: '60px', marginBottom: '60px' }}>
           <span className='bold underline large'>Recycle DC</span>
           </Typography>
-          <Typography>
+          <Typography sx={{ marginTop: '60px', marginBottom: '60px' }}>
             <span className='regular '>We can do much more as a city than we can do one-by-one. </span>
             <span className='regular bold green'>Next-Gen Progress </span>
             <span className='regular '>comes from advocacy, enforcement, and regulations.</span>
