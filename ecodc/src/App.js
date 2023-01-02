734872e926a58b35d7111d011a3ff0b2ca479337
Milo_Lynch <Milobravman20@gmail.com>
2023-01-02 17:31:25 -0500

    messing with components

--- ecodc/src/App.js
+++ ecodc/src/App.js
@@ -2,15 +2,13 @@
 import './App.css';
 import Home from './Home';
 import Recycle from './Components/Recycle';
-import { Routes, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/Recycle" element={<Recycle/>}/>
-      </Routes>
+        <Route exact path='/' component={<Home/>}/>
+        <Route path="/Recycle" component={<Recycle/>}/>
     </div>
   );
 }
