9e28a06cdf5a4a043d9301e5a48cd535efa698d0
Milo_Lynch <Milobravman20@gmail.com>
2023-01-04 17:24:20 -0500

    first commit of the day

--- ecodc/src/App.js
+++ ecodc/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Home from './Home';
 import Recycle from './Components/Recycle';
-import { Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-        <Route path='/' component={<Home/>}/>
-        <Route path="/Recycle" component={<Recycle/>}/>
+      <Routes>
+        <Route path='/EcoDC' element={<Home/>}/>
+        <Route path="/Recycle" element={<Recycle/>}/>
+      </Routes>
     </div>
   );
 }
--- ecodc/src/index.js
+++ ecodc/src/index.js
@@ -4,14 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, BrowserRouter } from 'react-router-dom';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
     ,document.getElementById('root'));
 
